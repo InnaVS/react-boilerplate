8611ee7e5f6310f977d21e65338db3a5e66a2ff5
Inna_Sokolenko <sokolenkoinna5@gmail.com>
2018-01-19 18:20:21 +0200

    Little changes in formatting of Childcontainer.

--- src/containers/ChildContainer.js
+++ src/containers/ChildContainer.js
@@ -2,15 +2,17 @@ import React, {Component} from 'react';
 
 class ChildContainer extends Component {
   render() {
-    return (<div>
-      <h5>
-        <a
-          className="center"
-          href="/">
-          <i className="fa fa-fw fa-lg fa-hand-o-left"/>
-        </a>
-      </h5>
-    </div>);
+    return (
+      <div>
+        <h5>
+          <a
+            className="center"
+            href="/">
+            <i className="fa fa-fw fa-lg fa-hand-o-left"/>
+          </a>
+        </h5>
+      </div>
+    );
   }
 }
 
